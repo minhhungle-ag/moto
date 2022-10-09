7bfaa0acb98341d0f9197aaac696d9deda8d6f13
minhhungle-ag <haless132@gmail.com>
2022-10-09 09:01:10 +0700

    cart form

--- src/components/formField/ColorField.jsx
+++ src/components/formField/ColorField.jsx
@@ -1,49 +1,76 @@
-import Radio from '@mui/material/Radio'
-import { Box, Stack } from '@mui/material'
+import { Stack, Tooltip } from '@mui/material'
 import FormControl from '@mui/material/FormControl'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import FormLabel from '@mui/material/FormLabel'
+import Radio from '@mui/material/Radio'
 import RadioGroup from '@mui/material/RadioGroup'
 import * as React from 'react'
+import { useController } from 'react-hook-form'
 
-export default function ColorField({ optionList, label, defaultValue }) {
+export default function ColorField({ name, control, optionList, label }) {
+  const [selectedIdx, setSelectedIdx] = React.useState(0)
+
+  const {
+    field: { value, onChange, onBlur, ref },
+  } = useController({
+    name,
+    control,
+  })
+
+  function handleChange(idx) {
+    setSelectedIdx(idx)
+  }
+
   return (
     <FormControl fullWidth>
       <FormLabel>{label}</FormLabel>
-      <RadioGroup defaultValue={defaultValue} name="radio-buttons-group">
+      <RadioGroup
+        name={name}
+        onChange={onChange}
+        onBlur={onBlur}
+        inputRef={ref}
+        value={value || optionList[0]?.value || ''}
+      >
         <Stack direction="row">
           {Array.isArray(optionList) &&
             optionList.length > 0 &&
             optionList.map((option, idx) => (
-              <FormControlLabel
-                key={idx}
-                value={option.value}
-                sx={{
-                  p: 1,
-                  border: '1px solid white',
-                  borderRadius: '50%',
-                }}
-                control={
-                  <Radio
-                    sx={{
-                      height: 40,
-                      width: 40,
-                      color: option.color,
-                      bgcolor: option.color,
+              <Tooltip title={option.color} arrow>
+                <FormControlLabel
+                  onClick={() => handleChange(idx)}
+                  key={idx}
+                  value={option.value}
+                  sx={{
+                    p: 1,
+                    border: '2px solid',
+                    borderRadius: '50%',
+                    borderColor: selectedIdx === idx ? 'grey.100' : 'grey.700',
 
-                      '&.Mui-checked': {
-                        border: '4px solid',
+                    '&:hover': {
+                      borderColor: 'grey.300',
+                    },
+                  }}
+                  control={
+                    <Radio
+                      sx={{
+                        height: 22,
+                        width: 22,
+
                         color: option.color,
-                        borderColor: 'primary.main',
-                      },
-
-                      '&:hover': {
                         bgcolor: option.color,
-                      },
-                    }}
-                  />
-                }
-              />
+
+                        '&.Mui-checked': {
+                          color: option.color,
+                        },
+
+                        '&:hover': {
+                          bgcolor: option.color,
+                        },
+                      }}
+                    />
+                  }
+                />
+              </Tooltip>
             ))}
         </Stack>
       </RadioGroup>
--- src/features/DiplomaticIt/components/AddToCardForm.jsx
+++ src/features/DiplomaticIt/components/AddToCardForm.jsx
@@ -1,22 +1,54 @@
 import { Box, Button, Divider, Stack, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import ColorField from '../../../components/formField/ColorField'
-
+import { yupResolver } from '@hookform/resolvers/yup'
 import { InputField } from '../../../components/formField/InputField'
 import { SelectField } from '../../../components/formField/SelectField'
+import * as yup from 'yup'
+
+const schema = yup.object({
+  amount: yup
+    .number()
+    .typeError('Amount is required and must be more than 0')
+    .integer('Amount must be integer')
+    .moreThan(0, 'Amount is required and must be more than 0'),
+})
 
 function AddToCardForm({ colorList, chassisList, optionList }) {
+  const [selectedChassis, setSelectedChassis] = useState({
+    idx: 0,
+    label: chassisList[0].label || '',
+    value: chassisList[0].label || '',
+  })
+
   const { control, handleSubmit } = useForm({
-    mode: 'all',
-    defaultValue: {
+    defaultValues: {
       brake_pads: 'Metallic',
-      amount: 0,
+      amount: 1,
+      color: 'black',
     },
+    mode: 'all',
+    resolver: yupResolver(schema),
   })
 
   function handleFormSubmit(formValues) {
-    //
+    const data = {
+      brake_pads: formValues.brake_pads,
+      amount: formValues.amount,
+      color: formValues.color,
+      chassis: selectedChassis.value,
+    }
+
+    console.log('formValues: ', data)
+  }
+
+  function handleChassisChange({ item, idx }) {
+    setSelectedChassis({
+      idx: idx,
+      label: item.label,
+      value: item.value,
+    })
   }
 
   return (
@@ -26,7 +58,7 @@ function AddToCardForm({ colorList, chassisList, optionList }) {
           Color
         </Typography>
 
-        <ColorField optionList={colorList} defaultValue="black" />
+        <ColorField name="color" control={control} optionList={colorList} />
       </Box>
 
       <Box>
@@ -34,13 +66,19 @@ function AddToCardForm({ colorList, chassisList, optionList }) {
           Chassis
         </Typography>
 
-        {/* <Stack direction="row" alignItems="center" spacing={2}>
+        <Stack direction="row" alignItems="center" spacing={2}>
           {chassisList.map((item, idx) => (
-            <Button variant="outlined" key={idx} color="inherit">
+            <Button
+              variant="outlined"
+              key={idx}
+              color="inherit"
+              onClick={() => handleChassisChange({ item, idx })}
+              sx={{ borderColor: selectedChassis.idx === idx ? 'primary.main' : 'inherit' }}
+            >
               {item.label}
             </Button>
           ))}
-        </Stack> */}
+        </Stack>
       </Box>
 
       <Box>
@@ -51,13 +89,14 @@ function AddToCardForm({ colorList, chassisList, optionList }) {
 
       <Box>
         <Typography variant="body2">Amount</Typography>
-
         <InputField control={control} name="amount" type="number" />
       </Box>
 
       <Divider />
 
-      <Button variant="outlined">Add to card</Button>
+      <Button type="submit" variant="contained">
+        Add to cart
+      </Button>
     </Stack>
   )
 }
